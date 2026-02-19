48d24393def936c9cc9d1133c4b90910daf92674
Hardik Singh <hardiksingh467@gmail.com>
2026-02-19 22:36:20 +0530

    variables and input completed inn python

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Contributors to this repository
 1. Hardik Singh
+
+
+## Python Basics
+Start by Procedural Programming where we will add a Python File and execute that Python file through terminal
---
+++ automate_tut.py
@@ -0,0 +1,26 @@
+# Mathematical Operations in Python
+print(2 + 2)
+print(2 - 3)
+print(2 * 2)
+print(2 / 2)
+print(2 % 2)
+print(2 ** 2)
+print("Alice" + "Bob")
+print("Hello" + "!" * 10)
+
+# Variables in Python
+
+variableName = 10
+
+print("variable Name is ", variableName)
+
+variableName = "Hardik"
+
+print("variableName is now", variableName)
+
+
+# Taking an Input from the User'
+
+myName = input("What is your name? ")
+
+print("Hello", myName)
